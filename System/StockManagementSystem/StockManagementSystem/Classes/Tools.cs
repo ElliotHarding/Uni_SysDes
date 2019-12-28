593d99978cd35d514916e4a136d89271b95615e9
unknown <elliot.one.harding@gmail.com>
2019-12-28 17:43:53 +0100

    Updated hashing function

--- System/StockManagementSystem/StockManagementSystem/Classes/Tools.cs
+++ System/StockManagementSystem/StockManagementSystem/Classes/Tools.cs
@@ -11,39 +11,9 @@ namespace StockManagementSystem.Classes
     {
         public static string passwordHash(string password)
         {
-            //return hash(password, 10, 10, 8);
-            return hash_(password);
-        }
-
-        private static string hash_(string password)
-        {
             var md5 = new MD5CryptoServiceProvider();
             var md5data = md5.ComputeHash(Encoding.ASCII.GetBytes(password));
             return Convert.ToBase64String(md5data);
-            //var hashedPassword = ASCIIEncoding.GetString(md5data);
-            //return hashedPassword;
-        }
-
-        private static string hash(string password, int iterations, int hashSize, int saltSize)
-        {
-            // Create salt
-            byte[] salt;
-            new RNGCryptoServiceProvider().GetBytes(salt = new byte[saltSize]);
-
-            // Create hash
-            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
-            var hash = pbkdf2.GetBytes(hashSize);
-
-            // Combine salt and hash
-            var hashBytes = new byte[saltSize + hashSize];
-            Array.Copy(salt, 0, hashBytes, 0, saltSize);
-            Array.Copy(hash, 0, hashBytes, saltSize, hashSize);
-
-            // Convert to base64
-            var base64Hash = Convert.ToBase64String(hashBytes);
-
-            // Format hash with extra information
-            return string.Format("$CH$V1${0}${1}", iterations, base64Hash);
-        }
+        }       
     }
 }

292ec95247c899f32cde81796cd90e6f46eb2a81
Hamid Mujtaba <N0683858@my.ntu.ac.uk>
2020-02-07 08:34:42 +0000

    Update Login.java

--- Mobile App/app/src/main/java/com/example/ntustores/Login.java
+++ Mobile App/app/src/main/java/com/example/ntustores/Login.java
@@ -45,53 +45,53 @@ public class Login extends AppCompatActivity {
 
 
         // Declare Database variables
-        ip = "000.000.000.00";
-        db = "den1.mssql7.gear.host";
-        un = "smdatabase";
-        pass = "Jj80-f1I!M3c";
+        // ip = "000.000.000.00";
+        // db = "den1.mssql7.gear.host";
+        // un = "smdatabase";
+        // pass = "Jj80-f1I!M3c";
 
 
         // onClick() listener for login btn
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String TempUsername = userName.getText().toString();
-                String TempPassword = password.getText().toString();
-                if(TempUsername.equals("") || TempPassword.equals(""))
-                {
-                    showErrorDialog("Error signing in! Please check email and password!");
-                }
-                else
-                {
-                    try {
-                        con = connectionClass(un, pass, db, ip);
-                        if (con == null)
-                        {
-                            showErrorDialog("Check your internet connection!");
-                        }
-                        else
-                        {
-                            String query = "select * from User where nNumber= '" + TempUsername.toString() + "' and password = '" + TempPassword.toString();
-                            Statement statement = con.createStatement();
-                            ResultSet rs = statement.executeQuery(query);
-                            if(rs.next())
-                            {
-                                Log.d("SQL Connection", "Login Successful!");
-                                con.close();
-                            }
-                            else
-                            {
-                                Log.d("SQL Connection", "Login NOT Successful!");
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Log.d("SQL Connection", "Exception: " + ex.getMessage());
-                    }
-                }
-            }
-        });
+        // loginBtn.setOnClickListener(new View.OnClickListener() {
+        //     @Override
+        //     public void onClick(View v) {
+        //         String TempUsername = userName.getText().toString();
+        //         String TempPassword = password.getText().toString();
+        //         if(TempUsername.equals("") || TempPassword.equals(""))
+        //         {
+        //             showErrorDialog("Error signing in! Please check email and password!");
+        //         }
+        //         else
+        //         {
+        //             try {
+        //                 con = connectionClass(un, pass, db, ip);
+        //                 if (con == null)
+        //                 {
+        //                     showErrorDialog("Check your internet connection!");
+        //                 }
+        //                 else
+        //                 {
+        //                     String query = "select * from User where nNumber= '" + TempUsername.toString() + "' and password = '" + TempPassword.toString();
+        //                     Statement statement = con.createStatement();
+        //                     ResultSet rs = statement.executeQuery(query);
+        //                     if(rs.next())
+        //                     {
+        //                         Log.d("SQL Connection", "Login Successful!");
+        //                         con.close();
+        //                     }
+        //                     else
+        //                     {
+        //                         Log.d("SQL Connection", "Login NOT Successful!");
+        //                     }
+        //                 }
+        //             }
+        //             catch (Exception ex)
+        //             {
+        //                 Log.d("SQL Connection", "Exception: " + ex.getMessage());
+        //             }
+        //         }
+        //     }
+        // });
 
 
     }
@@ -102,34 +102,34 @@ public class Login extends AppCompatActivity {
 //        startActivity(intent);
 //    }
 
-    @SuppressLint("NewApi")
-    public Connection connectionClass(String user, String pass, String database, String server)
-    {
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        Connection connection = null;
-        String ConnectionURL;
-        try
-        {
-            Log.d("SQL Connection", "Trying to connect");
-            Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            ConnectionURL = "jdbc:jtds:sqlserver://" + server + database + ";user=" + user + ";password=" + pass + ";";
-            connection = DriverManager.getConnection(ConnectionURL);
-        }
-        catch (SQLException se)
-        {
-            Log.e("Error 1: ", se.getMessage());
-        }
-        catch (ClassNotFoundException e)
-        {
-            Log.e("Error 2: ", e.getMessage());
-        }
-        catch (Exception e)
-        {
-            Log.e("Erro 3: ", e.getMessage());
-        }
-        return  connection;
-    }
+    // @SuppressLint("NewApi")
+    // public Connection connectionClass(String user, String pass, String database, String server)
+    // {
+    //     StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+    //     StrictMode.setThreadPolicy(policy);
+    //     Connection connection = null;
+    //     String ConnectionURL;
+    //     try
+    //     {
+    //         Log.d("SQL Connection", "Trying to connect");
+    //         Class.forName("net.sourceforge.jtds.jdbc.Driver");
+    //         ConnectionURL = "jdbc:jtds:sqlserver://" + server + database + ";user=" + user + ";password=" + pass + ";";
+    //         connection = DriverManager.getConnection(ConnectionURL);
+    //     }
+    //     catch (SQLException se)
+    //     {
+    //         Log.e("Error 1: ", se.getMessage());
+    //     }
+    //     catch (ClassNotFoundException e)
+    //     {
+    //         Log.e("Error 2: ", e.getMessage());
+    //     }
+    //     catch (Exception e)
+    //     {
+    //         Log.e("Erro 3: ", e.getMessage());
+    //     }
+    //     return  connection;
+    // }
 
     private void showErrorDialog(String message)
     {

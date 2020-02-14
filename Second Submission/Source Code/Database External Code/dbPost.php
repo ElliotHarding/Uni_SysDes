6545d1dd84591f14e380c7ecf0add6211db351e4
ElliotHarding <33599761+ElliotHarding@users.noreply.github.com>
2020-02-14 18:30:51 +0000

    Added php files

---
+++ Second Submission/Source Code/Database External Code/dbPost.php
@@ -0,0 +1,39 @@
+<?php
+	$serverName = "den1.mssql7.gear.host"; 
+	$uid = "smdatabase";   
+	$pwd = "Jj80-f1I!M3c";  
+	$databaseName = "smdatabase"; 
+ 
+	$conn = sqlsrv_connect($serverName, array( "UID"=>$uid, "PWD"=>$pwd, "Database"=>$databaseName));  
+
+	$pid = $_REQUEST['pid'];
+	$nNumber = $_REQUEST['nNumber'];
+	$query = "IF EXISTS (SELECT * FROM SCANS WHERE nNumber = '" . $nNumber . "')
+						BEGIN
+							UPDATE SCANS SET pid = '" . $pid . "' WHERE nNumber = '" . $nNumber . "';
+						END
+						ELSE
+						BEGIN
+						   INSERT INTO SCANS VALUES ('" . $nNumber . "', '" . $pid . "')
+						END";
+						
+	$stmt = sqlsrv_query($conn, $query);  					
+	if ( $stmt )  
+	{  
+		/* Iterate through the result set printing a row of data upon each iteration.*/ 
+		while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC))  
+		{  
+			$output[]=$row;
+		}
+		echo str_replace('"', '', json_encode($output));
+	}   
+	else   
+	{  
+		 echo "Error \n";  
+		 die( print_r( sqlsrv_errors(), true));  
+	}  
+	
+	/* Free statement and connection resources. */  
+	sqlsrv_free_stmt($stmt);  
+	sqlsrv_close( $conn);  
+?>
